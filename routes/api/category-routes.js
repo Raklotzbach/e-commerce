fec051c582019228bbd2f79a9ec87541142e7357
Ryan Klotzbach <ryanklotzbach@Ryans-iMac.local>
2022-07-30 12:35:53 -0700

    updated api code

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,11 +5,33 @@ const { Category, Product } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all categories
+  try {
+    const categoriesData = await Category.findAll ({
+      include: [ {model:Product}]
+    })
+    res.status(200).json(categoriesData);
+  } catch(err) {
+    res.status(500).json(err);
+  }
   // be sure to include its associated Products
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
+  try {
+    const categoryData = await Category.findBy Pk(req.params.id, {
+      include: [ {model: Product}]
+    })
+
+    if (!categoryData) {
+      res.status(404).json({ message: "No category found"});
+    } else {
+      res.status(200).json(categoryData)
+    }
+
+  } catch (err) {
+    res.status(500).json(err)
+  }
   // be sure to include its associated Products
 });
 
